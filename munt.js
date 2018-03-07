a3dbe3ba2708a9e0e72f245bae95e00d48b1af6d
Sven van der Zee <svenzee@hotmail.nl>
2018-03-07 09:01:34 +0100

    Use iframe instead of popup

--- munt.js
+++ munt.js
@@ -2,7 +2,25 @@
  * Created by Sven on 11-12-2017.
  */
 
-var host = "https://getmunt.com";
+var e = document.createElement("iframe");
+setAttributes(e, {'id' : 'MuntIFrame', 'allowfullscreen' : '', 'onload' : 'this.width=window.innerWidth;',  'src' : 'https://getmunt.com/checkout', 'style' : 'z-index: 2147483647;' +
+'    display: block;' +
+'    border: 0px none transparent;' +
+'    overflow-x: hidden;' +
+'    overflow-y: auto;' +
+'    visibility: hidden;' +
+'    margin: 0px;' +
+'    padding: 0px;' +
+'    position: fixed;' +
+'    left: 0px;' +
+'    top: 0px;' +
+'    height: 100%;'});
+
+window.onload = function () {
+
+    document.body.appendChild(e);
+
+};
 
 function checkout(params, callback) {
 
@@ -12,7 +30,18 @@ function checkout(params, callback) {
 
     if(params.token !== undefined){
 
-        window.open(host + "/checkout/"+params.token, "checkout", "");
+        setAttributes(e, {'src' : 'https://getmunt.com/checkout/' + params.token, 'onload' : 'this.width=window.innerWidth;', 'style' : 'z-index: 2147483647;' +
+        '    display: block;' +
+        '    border: 0px none transparent;' +
+        '    overflow-x: hidden;' +
+        '    overflow-y: auto;' +
+        '    visibility: visible;' +
+        '    margin: 0px;' +
+        '    padding: 0px;' +
+        '    position: fixed;' +
+        '    left: 0px;' +
+        '    top: 0px;' +
+        '    height: 100%;'});
 
         var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
         var eventer = window[eventMethod];
@@ -54,6 +83,32 @@ function checkout(params, callback) {
         response = {error: true, message: "missing payment_id"};
         callback(response);
 
+    }
+
+}
+
+function closeIFrame() {
+
+    setAttributes(e, {'src' : host + '/checkout/', 'onload' : 'this.width=window.innerWidth;', 'style' : 'z-index: 2147483647;' +
+    '    display: block;' +
+    '    border: 0px none transparent;' +
+    '    overflow-x: hidden;' +
+    '    overflow-y: auto;' +
+    '    visibility: hidden;' +
+    '    margin: 0px;' +
+    '    padding: 0px;' +
+    '    position: fixed;' +
+    '    left: 0px;' +
+    '    top: 0px;' +
+    '    width: 100%;' +
+    '    height: 100%;'});
+    document.body.appendChild(e);
+
+}
+
+function setAttributes(e, attrs) {
+    for(var a in attrs) {
+        e.setAttribute(a, attrs[a]);
     }
 
 }
